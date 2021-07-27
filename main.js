cd4d00672c7033578a7680bab2bd2f8da890ecf1
Arshad Sarkar Hanif <65861090+mashbd@users.noreply.github.com>
2021-07-27 16:57:06 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.animate', {
+    // Waits 1000ms after typing "First"
+    strings: ['Arshad Sarkar Hanif', ' a Web Designer', 'a Future WordPress Developer'],
+    typeSpeed: 90,
+    backSpeed: 90,
+    backDelay: 800,
+    loop: true,
+  });
